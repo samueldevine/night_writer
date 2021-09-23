c5cd928c16392ae99751f42ec6fb4cae3b2c8b8a
Sam Devine <samuel.will.devine@gmail.com>
2021-09-23 17:28:18 -0600

    Added ARGV to accept command-line arguments

---
+++ lib/night_writer.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+# This is the main file for Night Writer! This project translates plain text
+# files to braile, using 0 to represent empty spaces and . to represent raised
+# dots in each block.
+
+input_array = ARGV
