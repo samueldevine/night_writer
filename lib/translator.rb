6e1d21266e7f03847b7afd5bd869ed47f49726dc
Sam Devine <samuel.will.devine@gmail.com>
2021-09-23 19:19:24 -0600

    Start building Translator class

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,5 +1,21 @@
+# frozen_string_literal: true
+
+# The Translator class creates a dictionary using the included .csv file and
+# uses it to translate messages into braille
+
+require 'csv'
+
 class Translator
-  def initialize
+  def initialize(path)
+    @dictionary = generate(path)
+  end
+
+  def generate(path)
+    read_csv(path).map do |row|
+      # need to create read_csv module
+      # need to read columns, not rows
+      Letter.new(row)
+    end
   end
 
   def lookup(letter)
